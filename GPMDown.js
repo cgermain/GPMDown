880c1ede3992bd0a9bc6d8d422f8ea3dc762d92b
Colin Germain <cgermain@gmail.com>
2017-03-21 08:33:30 -0400

    Simplified opening and added 2nd link to download for each GPM file.

--- GPMDown.js
+++ GPMDown.js
@@ -1,4 +1,4 @@
-// this is the code which will be injected into a given page...
+// this scrapes the page for any mention of GPM* and then creatues links to the two pages in GPM to download excel sheets
 
 (function() {
 	var regex = /(GPM\d+)/g;
@@ -12,45 +12,23 @@
 	}
 
 	gpm_files = Array.from(new Set(gpm_files));
-
+	console.log(gpm_files);
+	
 	var urls = gpm_files.map(function(gpm_filename){
 		var url_prefix = "http://localhost:8082/thegpm-cgi/plist.pl?filter=&proex=-1&npep=0&nohead=yes&path=/gpm/archive/";
 		var url_suffix = ".xml&order=&d=&sheet=1&display_select=2";
 		return url_prefix + gpm_filename + url_suffix;
-	})
+	});
 
-	console.log(urls);
+	var protein_urls = gpm_files.map(function(gpm_filename){
+		var protein_url_prefix = "http://localhost:8082/thegpm-cgi/plist.pl?filter=&proex=-1&npep=0&nohead=yes&path=/gpm/archive/";
+		var protein_url_suffix = ".xml&order=&d=&sheet=1&display_select=2";
+		return protein_url_prefix + gpm_filename + protein_url_suffix;
+	});
+
 	for (i = 0; i < urls.length ; i++){
-		var a = document.createElement('a');
-		var filename = urls[i];
-		a.download = filename;
-		a.style.display = 'none';
-		document.body.appendChild(a);
-		a.click();
-		console.log("clicked: " + filename)
-		delete a;
+		window.open(urls[i]);
+		window.open(protein_urls[i]);
 	}
-
-	//TODO-Get zip working
-	// var zip = new JSZip();
-	// var count = 0;
-	// var date = new Date();
-	// var zipFilename = "GPM_xls_archive_" + date.getYear() + date.getMonth() + date.getDate() + ".zip";
-
-	// urls.forEach(function(url){
-	//   var filename = gpm_files[count]+".xls";
-	//   // loading a file and add it in a zip file
-	//   JSZipUtils.getBinaryContent(url, function (err, data) {
-	//      if(err) {
-	//         throw err; // or handle the error
-	//      }
-	//      zip.file(filename, data, {binary:true});
-	//      count++;
-	//      if (count == urls.length) {
-	//        var zipFile = zip.generate({type: "blob"});
-	//        saveAs(zipFile, zipFilename);
-	//      }
-	//   });
-	// });
 
 })();
